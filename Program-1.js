1c2c2701487f4f0fc3a476fb0d84e33558c6009d
Vasantha135 <ragimekalapallivasantha@gmail.com>
2025-09-01 10:38:28 +0530

    Add files via upload

---
+++ Program-1.js
@@ -0,0 +1,26 @@
+
+class Calculator {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    operate(operation) {
+        switch (operation) {
+            case 'add':
+                return this.a + this.b;
+            case 'subtract':
+                return this.a - this.b;
+            case 'multiply':
+                return this.a * this.b;
+            case 'divide':
+                return this.b !== 0 ? this.a / this.b : 'Error: Division by zero';
+            default:
+                return 'Invalid operation';
+        }
+    }
+}
+
+const calc = new Calculator(10, 5);
+console.log(calc.operate('add'));        
+console.log(calc.operate('divide'));    
